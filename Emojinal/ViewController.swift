413f0971c6b636bfa3f620fd4e3e03c3b860946d
Shirui Zhong <apple@Apples-MacBook-Pro-66.local>
2019-06-20 16:08:44 -0500

    complete iteration 1

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -14,7 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController()
+        alertController.addAction(UIAlertAction(title:
+            "Today's such a silly day", style: UIAlertAction.Style.default, handler: nil))
+        
+    
+    present(alertController, animated: true, completion: nil)
+    }
 }
 
