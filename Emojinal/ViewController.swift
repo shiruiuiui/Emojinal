6940d1d2735d783864543dc591601d3a978c6fe9
Shirui Zhong <apple@Apples-MacBook-Pro-66.local>
2019-06-21 10:23:38 -0500

    complete iteration 3

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -10,17 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emojis = ["🤪": "silly", "🥴": "mood"]
+    let customMessage = ["silly": ["anna ou-", "and i oop", "i wanna be a cowboy baby", "oh shit its a raAAAt"], "mood": ["uR diSrEsPeCTIng A fUtUre uS ArMy SOldIer","give me ur facking maney", "ah hah ha"]]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+
     @IBAction func showMessage(sender: UIButton) {
+        let selectedEmoticon = sender.titleLabel?.text
+        let num = Int.random(in: 0..<3)
+        let emojiMessage = customMessage[emojis[selectedEmoticon!]!]?[num]
         let alertController = UIAlertController()
         alertController.addAction(UIAlertAction(title:
-            "Today's such a silly day", style: UIAlertAction.Style.default, handler: nil))
+            emojiMessage, style: UIAlertAction.Style.default, handler: nil))
+
         
     
     present(alertController, animated: true, completion: nil)
+        
     }
 }
 
